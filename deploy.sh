0540f9eeff32dd7200383d4a5222b44429b0a394
SoftwareEngineer01 <sistemsistemaweb@gmail.com>
2021-01-16 21:41:45 -0500

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:SoftwareEngineer01/giving.git master:gh-pages
+git push -f git@github.com:SoftwareEngineer01/front-giving.git master:gh-pages
 
 cd -
